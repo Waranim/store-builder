56b18219b8d6a05873aefe2af3776dfb72af3ae3
Waranim <129619485+Waranim@users.noreply.github.com>
2025-03-25 16:44:50 +0500

    Init commit

---
+++ src/main/java/xyz/waranim/storebuilder/StoreBuilderApplication.java
@@ -0,0 +1,13 @@
+package xyz.waranim.storebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/waranim/storebuilder/StoreBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.waranim.storebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
